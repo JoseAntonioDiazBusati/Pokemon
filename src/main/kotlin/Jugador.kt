bab24e9db684175da014efb26b4baf424d9b0e6f
JoseAntonio <jadiaz@iesrafaelalberti.es>
2023-03-27 16:51:36 +0200

    jugador

--- src/main/kotlin/Jugador.kt
+++ src/main/kotlin/Jugador.kt
@@ -2,7 +2,11 @@ class Jugador(var pokemon: Pokemon) {
     fun obtenerPokemon(): Pokemon {
         return pokemon
     }
-
+    /**
+     * Estos son los atributos necesarios para crear un jugador
+     * @property pokemon define a cada jugador
+     * @author Jose Antonio
+     */
     fun establecerPokemon(pokemon: Pokemon) {
         this.pokemon = pokemon
     }
